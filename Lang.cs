a1784008da8b9be9499689bd09f09b52f9576d73
Tony <TonyRL@users.noreply.github.com>
2017-05-09 17:14:17 +0800

    spells draw and farm done

--- Lang.cs
+++ Lang.cs
@@ -26,7 +26,7 @@
         public static string PRESSF5TORELOADMODE = "PRESS F5 TO RELOAD MODE";
         public static string OKTWorbwalker = "OKTW orbwalker";
         public static string ORBWALKERMODE = "ORBWALKER MODE";
-
+		//Spells DRAW and FARM
         public static string spellsDrawAndFarm = "Spells DRAW and FARM";
         public static string drawSpellRange = "Draw spell range";
         public static string drawOnlyReadySpells = "Draw only ready spells";
@@ -38,7 +38,6 @@
         public static string importantBuffTimer = "Important buff timer";
         public static string manaPercent = " mana %";
         public static string utilityAndDraws = "Utiliy & draws";
-
         //Humanizer
         public static string sbwV2Humanizer = "SBW V2 humanizer";
         public static string enableHumanizer = "Enable humanizer";
@@ -169,18 +168,127 @@
                 PRESSF5TORELOADMODE = "請按下F5重新載入";
                 OKTWorbwalker = "OKTW 走砍";
                 ORBWALKERMODE = "走砍模式";
-
-                spellsDrawAndFarm = "技能范围以及发育";
-                drawSpellRange = "显示技能范围";
-                drawOnlyReadySpells = "仅显示已经CD好的今年刚";
-                farmToggleMode = "发育模式按键(按住激活)";
-                scrollDown = "鼠标中键滚轮";
-                scrollPress = "鼠标中键按下";
-                keyToggle = "按键(按住激活)";
-                spellsFarm = "技能发育状态";
-                importantBuffTimer = "重要的buff持续时间";
-                manaPercent = " 最低蓝量比 %";
-                utilityAndDraws = "功能 & 显示";
+				//Spells DRAW and FARM
+                spellsDrawAndFarm = "技能顯示及農兵";
+                drawSpellRange = "顯示技能範圍";
+                drawOnlyReadySpells = "僅顯示可用技能";
+                farmToggleMode = "農兵模式開關";
+                scrollDown = "滾動滑鼠滾輪";
+                scrollPress = "按下滑鼠滾輪";
+                keyToggle = "開關熱鍵";
+                spellsFarm = "技能農兵";
+                importantBuffTimer = "重要增益計時器";
+                manaPercent = " 最低魔力 %";
+                utilityAndDraws = "功能腳本及顯示";
+				//Humanizer
+				public static string sbwV2Humanizer = "SBW V2 humanizer";
+				public static string enableHumanizer = "Enable humanizer";
+				public static string blockActionOutScreen = "Bloock auction out screen";
+				public static string minimumClickTimeMs = "Minimum Click Time ms";
+				public static string drawActionPerSecond = "Draw action per second";
+				//BaseUlt
+				public static string baseUlt = "BaseUlt";
+				public static string enableBaseUlt = "Enable BaseUlt";
+				public static string enableBaseUltNotification = "Enable BaseUlt notification";
+				public static string disableInComboMode = "Disable in COMBO mode";
+				public static string enemies = "Enemies";
+				//AutoLeveler
+				public static string autoLeveler = "Auto Leveler";
+				public static string autoLevlerMode = "Auto leveler mode";
+				public static string priority = "Priority";
+				public static string meta = "Meta";
+				public static string autoLevelerStart = "Auto leveler start";
+				//Activator
+				public static string activator = "Activator";
+				public static string autoWards = "Auto wards";
+				public static string onlyComboMode = "Only combo mode";
+				public static string autoPlaceWard = "Auto place ward";
+				public static string autoPlaceBlueTrinket = "Auto place blue trinket";
+				public static string autoVisionWardOracleLens = "Auto VisionWard, OracleLens";
+				public static string summonersHeal = "Summoners - Heal";
+				public static string heal = "Heal";
+				public static string allyHeal = "Ally Heal";
+				public static string itemsOffensive = "Items Offensive";
+				public static string botrk = "Botrk";
+				public static string botrkLifeSaver = "Botrk LifeSaver";
+				public static string ks = "KS";
+				public static string combo = "Combo";
+				public static string botrkCombo = "Botrk Comco";
+				public static string cutlass = "Cutlass";
+				public static string hextech = "Hextech";
+				public static string protobelt = "Protobelt";
+				public static string glp800 = "GLP800";
+				public static string yommus = "Yommus";
+				public static string yommusTwitchAshe = "Twitch R, Ashe Q";
+				public static string others = "Others";
+				public static string hydra = "Hydra";
+				public static string hydraTitanic = "Hydra titanic";
+				public static string frostQueen = "Frost Queen";
+				public static string itemsDefensives = "Items - Defensives";
+				public static string consumable = "Consumable";
+				public static string faceOfTheMountain = "FaceOfTheMountain";
+				public static string solari = "Solari";
+				public static string randuin = "Randuin";
+				public static string seraph = "Seraph";
+				public static string zhonya = "Zhonya";
+				public static string zhonyaSpells = "Zhonya Spells";
+				public static string cleansers = "Cleansers";
+				public static string cleanseEnable = "Cleanse";
+				public static string useOnlyUnderPercentHP = "Use Only Under % HP";
+				public static string delayInMs = "Delay in ms";
+				public static string buffType = "Buff Type";
+				public static string stun = "Stun";
+				public static string charm = "Charm";
+				public static string suppression = "Suppression";
+				public static string blind = "Blind";
+				public static string snare = "Snare";
+				public static string fear = "Fear";
+				public static string taunt = "Tannt";
+				//UtilityAndDraws
+				public static string minimap = "Minimap";
+				public static string showLastEnemyPosition = "Show last enemy position";
+				public static string showPredictedPositionCircles = "Show predicted position circles";
+				public static string showLastEnemyDirection = "Show last enemy direction";
+				public static string uiAvatars = "UI Avatars";
+				public static string showEnemyAvatars = "Show enemy avatars";
+				public static string bigHdAvatars = "Big HD Avatars";
+				public static string positionX = "Position X";
+				public static string positionY = "Position Y";
+				public static string spellsTracker = "Spells tracker";
+				public static string showSpellsTracker = "Show Spells tracker";
+				public static string showEnemySpells = "Show enemy spells";
+				public static string showSpellsLevel = "Show spells level(No Fps Friendly)";
+				public static string showAlliesSpells = "Show allies spells";
+				public static string showMySpells = "Show my spells";
+				public static string orbitalChampionsRadar = "Orbital champions radar";
+				public static string onlyJungler = "Only jungler";
+				public static string onlyEnemy = "Only enemy";
+				public static string damageIndicators = "Damage indicator";
+				public static string showEnemyWards = "Show enemy wards";
+				public static string showEnemyClicks = "Show enemy clicks";
+				public static string jungleTracker = "Jungle tracker";
+				//OKTW AIO 2
+				public static string oktwAio2 = "OKTW AIO 2";
+				public static string advanced = "Advanced";
+				public static string orbwalker = "Orbwalker";
+				public static string disableAutoAttacksInComboAfterLevel = "Disable auto attacks in combo after level(0 = always aa)";
+				public static string supportMoade = "Support mode";
+				public static string harrasInLaneClearMode = "Harras in lane clear mode";
+				public static string mana = "Mana";
+				public static string disableManaManagerInCombo = "Disable mana manager in combo";
+				public static string harrasManaPercent = "Harras mana % (-1 is auto)";
+				public static string masteries = "Masteries";
+				public static string doubleEdgedSword = "Double-Edged Sword 3%";
+				public static string merciless = "Mercilles 3%";
+				public static string onStartDraws = "On start draws";
+				public static string oktwLogoOnStart = "Oktw logo on start";
+				public static string supportedChampionsInfo = "Supported champions info";
+				public static string other = "Other (Please disable if game crashes)";
+				public static string onCreate1 = "OnCreate1 - Advanced damage prediciton";
+				public static string onCreate2 = "OnCreate2 - Advanced minimap detection";
+				public static string onCreate3 = "OnCreate3 - Minimap exploit";
+				public static string developerOptions = "Developer options";
+				public static string enableConsoleDebug = "Enable console debug";
             }
             else if (LangSet == translation.Polish)
             {
